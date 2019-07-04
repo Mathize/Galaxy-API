8e7fe96819d52b53ff7acc3aed84fd6d1219f860
mathize <matias.somonte@gmail.com>
2019-07-04 11:59:49 -0300

    New with put

--- routes/galaxias.js
+++ routes/galaxias.js
@@ -41,32 +41,29 @@ const crudGalaxias = (app) => {
 };
     
     //PUT - Update a register already exists
-//  updateCerveceria = function(req, res) {
-//  	Cerveceria.findById(req.params.id, function(err, cerveceria) {
-//  		cerveceria.id   = req.body.id;
-//  		cerveceria.slug    = req.body.slug;
-//  		cerveceria.nombreCer = req.body.nombreCer;
-//  		cerveceria.ubicacionCer  = req.body.ubicacionCer;
-//  		cerveceria.urlImagenCer = req.body.urlImagenCer;
-//  		cerveceria.promBebCer   = req.body.promBebCer;
-//  		cerveceria.promComCer = req.body.promComCer;
-//      cerveceria.lat   = req.body.lat;
-//  		cerveceria.lng   = req.body.lng;
-//  		cerveceria.save(function(err) {
-//  			if(!err) {
-//  				console.log('Updated');
-//  			} else {
-//  				console.log('ERROR: ' + err);
-//  			}
-//  			res.send(cerveceria);
-//  		});
-//  	});
-//  }
+  updateGalaxias = function(req, res) {
+  	Galaxia.findById(req.params.id, function(err, galaxias) {
+            galaxias.id =    req.body.id;
+            galaxias.galaxia = req.body.galaxia;
+            galaxias.tipo =  req.body.tipo;
+            galaxias.planetas =  req.body.planetas;
+            galaxias.planetasEnanos =   req.body.planetasEnanos;
+            galaxias.imagen =  req.body.imagen;
+  		galaxias.save(function(err) {
+  			if(!err) {
+  				console.log('Updated');
+  			} else {
+  				console.log('ERROR: ' + err);
+  			}
+  			res.send(galaxias);
+  		});
+  	});
+  }
 
     //Rutas de la API, asociadas a una función
     app.get('/galaxias', findAllGalaxias); 
     app.post('/galaxias', addGalaxia);
-//    app.put('/galaxias/:id', updateCerveceria) //actualiza una galaxia
+    app.put('/galaxias/:id', updateGalaxias) //actualiza una galaxia
 }
 
 module.exports = crudGalaxias;
